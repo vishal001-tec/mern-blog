15b9ef0d6b5f3da7cab969ce9e60264418d8f734
vishal Biradar <biradarvishal756@gmail.com>
2024-02-21 22:08:36 +0530

    update header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,12 @@
-import { Button, Navbar, NavbarToggle, TextInput } from 'flowbite-react';
+import { Avatar, Button, Dropdown, DropdownDivider, DropdownHeader, DropdownItem, Navbar, NavbarToggle, TextInput } from 'flowbite-react';
 import React from 'react';
 import { Link,useLocation } from 'react-router-dom';
 import {AiOutlineSearch} from 'react-icons/ai';
 import {FaMoon} from 'react-icons/fa';
+import {useSelector} from 'react-redux';
 export default function Header() {
     const path = useLocation().pathname;
+    const {currentUser} =useSelector(state=>state.user)
   return (
     <Navbar className=' border-b-2'>
     <Link to="/" className='self-center  whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -26,11 +28,36 @@ export default function Header() {
         <Button className=' w-12 h-10 hidden sm:inline' color=' gray' pill>
             <FaMoon></FaMoon>
         </Button>
+        {currentUser ? (
+            <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+                <Avatar
+                alt='user'
+                img={currentUser.profilePicture}
+                rounded
+                ></Avatar>
+            }
+            >
+                <Dropdown.Header>
+                    <span className=' block text-sm'>@{currentUser.username}</span>
+                    <span className=' block text-sm font-medium truncate'>@{currentUser.email}</span>
+
+                </Dropdown.Header>
+                <Link to={'/dashboard?tab=profile'}>
+                    <Dropdown.Item>Profile</Dropdown.Item>
+                </Link>
+                <Dropdown.Divider/>
+                <Dropdown.Item>Sign out</Dropdown.Item>
+            </Dropdown>
+        ):(
         <Link to='/sign-in'>
             <Button gradientDuoTone='purpleToBlue' outline>
                 SignIn
             </Button>
         </Link>
+        )}
         <Navbar.Toggle/>
         </div>
         <Navbar.Collapse>
@@ -54,8 +81,6 @@ export default function Header() {
                     Projects
                 </Link>
             </Navbar.Link>
-            
-
         </Navbar.Collapse>
    
     </Navbar>
